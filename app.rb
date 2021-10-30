ce798eaa1c16ac45fc04a67e7a2a2c48b8323a11
garrettdick <garrettd0511@gmail.com>
2021-10-30 11:10:29 -0500

    commit tree c753be83288c81e365fd2d4f558dca32f31501b8

---
+++ app.rb
@@ -0,0 +1,58 @@
+# puts "Hello world!"
+
+#testing to see if this is printed out in terminal when using the ruby .app.rb call
+# It totally did not :) This is the JavaScript equivalent of // to comment something out 
+
+
+## 'puts' is a built in Ruby Method that will outpupt a string of text to the terminal.
+# this is Rubys version of console.log()
+# In Ruby, adding parenthesies 
+# to invoke are optional as seen with the example above and below. 
+
+
+
+## to runt he application, enter the command: ruby filename.rb || where filename.rb is the relative path to the 
+# file you would like to run 
+# Example: ruby app.rb 
+# => Hello World!
+
+# puts("Hello World!")
+
+# ### Ruby Terminal Output Methods ####
+
+# Outputting Strings with puts and print
+
+# Ruby several methods for printing output to the terminal: puts, print, p, and pp.
+
+# print and puts do almost the same thing; the only difference is that puts will add a line break at the end of the string. Try updating the code in app.rb to the following:
+
+# # app.rb
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+# print "Hello World"
+# print "Hello World"
+# print "Hello World"
+
+# print [1,2,3]
+
+# # print seems to be the better way to go when viewing Arrays 
+
+# puts [1,2,3]
+
+# p [1,2,3]
+
+# puts [1,2,3].inspect
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1,2,3]
